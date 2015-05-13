82610527349a87502692465c433ab3a2b58d60f9
dkrompic <dkrompic@gmail.com>
2015-05-14 01:04:02 +0200

    Validation, tests and fixtures added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :price, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: true, format: {
+    with: /\.(gif|bmp)\z/i,
+    message: 'must be either gif or bmp'
+  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,30 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes marked as mandatory must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+  end
+
+  test "product price must be greater then or equal to 0.01" do
+    product = Product.new(title: "Price test",
+                          description: "Price validation testing")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price] 
+
+  end
+
+  test "product must have unique title" do
+    product = Product.new(title: products(:new_row).title,
+                          description: "Price validation testing",
+                          price: 11.00)
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+
+  end
+
 end
