e97774c44d71e9eb8948a7e334751233cde8998e
dkrompic <dkrompic@gmail.com>
2015-05-14 22:49:11 +0200

    Set root, fill in controller, view, css, caching

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,14 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :price, presence: true
   validates :title, uniqueness: true
+  validates_length_of :title, minimum: 6, message: "at least 6 characters" 
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :image_url, allow_blank: true, format: {
     with: /\.(gif|bmp)\z/i,
     message: 'must be either gif or bmp'
   }
+  
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
