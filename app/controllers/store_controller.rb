2cbab4dd1763219d41a73d59c619452cfdb45fc4
dkrompic <dkrompic@gmail.com>
2015-05-16 17:06:40 +0200

    Cart added

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @counter = increment_count
+  end
+  
+private  
+
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   validates :title, :description, :price, presence: true
   validates :title, uniqueness: true
   validates_length_of :title, minimum: 6, message: "at least 6 characters" 
@@ -10,5 +14,16 @@ class Product < ActiveRecord::Base
   
   def self.latest
     Product.order(:updated_at).last
+  end
+ 
+private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
   end
 end
